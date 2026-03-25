3c322f254802c3cc4f3e853a20fabceaa1ae5f37
Yash Jani <yashjani144@gmail.com>
2026-03-24 22:59:35 -0400

    start afresh

--- info.jsx
+++
@@ -1,3 +0,0 @@
-export const info = {
-    skills: ["C", "C++", "Java", "Rust", "R", "Python", "HTML/CSS", "JavaScript", "Typescript", "SQL", "MATLAB"],
-}
--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "@/styles/globals.css";  // Import global styles once here
-
-function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
